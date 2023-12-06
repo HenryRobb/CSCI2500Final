72275812ea1b6f2887452d58c6eedca233b06552
bialasj26 <152747531+bialasj26@users.noreply.github.com>
2023-12-05 21:27:20 -0500

    Add more example files

--- kram.sv
+++ kram.sv
@@ -1,21 +1,26 @@
-'timescale 1ns / 1ps
+// Adopted from https://www.chipverify.com/verilog/verilog-single-port-ram
+`timescale 1 ns / 1 ps
 
-module ram
-  #	(parameter ADDR_WIDTH = 12,
+module single_port_sync_ram
+  # (parameter ADDR_WIDTH = 12,
      parameter DATA_WIDTH = 8,
      parameter LENGTH = (1<<ADDR_WIDTH)
-    );
-  (input clk,
-   input[ADDR_WIDTH-1:0] addr,
-   inout [DATA_WIDTH-1:0] data,
-   input cs,
-   input we,
-   input oe
+    )
+
+  (   input clk,
+      input [ADDR_WIDTH-1:0] addr,
+      inout [DATA_WIDTH-1:0] data,
+      input cs,
+      input we,
+      input oe
   );
-  
-  reg [DATA_WIDTH-1:0] mem[LENGTH];
+
   reg [DATA_WIDTH-1:0] tmp_data;
+  reg [DATA_WIDTH-1:0] mem[LENGTH];
+
   always @ (posedge clk) begin
+    // Used when debugging a multiplication program
+    // if (addr=='h10D) $display("Product is %d", mem[addr]);
     if (cs & we)
       mem[addr] <= data;
   end
@@ -24,6 +29,6 @@ module ram
     if (cs & !we)
       tmp_data <= mem[addr];
   end
-  
+
   assign data = cs & oe & !we ? tmp_data : 'hz;
 endmodule
