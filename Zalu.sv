39a8fa53d10e230240a3d2a4ab8549464fad88c8
bialasj26 <152747531+bialasj26@users.noreply.github.com>
2023-12-05 20:40:13 -0500

    Get rid of Cin and Cout

--- Zalu.sv
+++ Zalu.sv
@@ -24,33 +24,30 @@ module not8(s, a);
   not(s[7], a[7]);
 endmodule
 
-module add8 (s, cout, ci, a, b);
+module add8 (s, a, b);
   input [7:0] a,b ; // port declarations
-  input ci ;
   output [7:0] s ; // vector
-  output cout ;
   wire [6:0] co ;
-    fadd a0 (co[0], s[0], a[0], b[0], ci);
+  reg throwAway;
+    fadd a0 (co[0], s[0], a[0], b[0], 0);
     fadd a1 (co[1], s[1], a[1], b[1], co[0]);
     fadd a2 (co[2], s[2], a[2], b[2], co[1]);
     fadd a3 (co[3], s[3], a[3], b[3], co[2]);
     fadd a4 (co[4], s[4], a[4], b[4], co[3]);
     fadd a5 (co[5], s[5], a[5], b[5], co[4]);
     fadd a6 (co[6], s[6], a[6], b[6], co[5]);
-    fadd a7 (cout, s[7], a[7], b[7], co[6]);
+    fadd a7 (throwAway, s[7], a[7], b[7], co[6]);
 endmodule
 
-module sub8 (s, cout, ci, a, b);
+module sub8 (s, a, b);
   input [7:0] a,b ; // port declarations
-  input ci ;
   output reg [7:0] s ; // vector
-  output cout ;
   reg [7:0] invv;
   not8 n0 (invv, b);//invert b
   reg [7:0] one = 00000001;
   reg[7:0] hold;
-  add8 a0(hold, cout, ci, invv, one);//add 1 to b
-  add8 a1(s, cout, ci, a, hold); //add a and negative b
+  add8 a0(hold, invv, one);//add 1 to b
+  add8 a1(s, a, hold); //add a and negative b
 endmodule
 
 
@@ -81,23 +78,21 @@ module or8(s, a, b);
   or(s[7], a[7], b[7]);
 endmodule
 
-module alu(s, cout, ci, a, b, aluMode);
+module alu(s, a, b, aluMode);
   input [7:0] a,b ; // port declarations
-  input ci ;
   input [3:0] aluMode; //switch between functions
   output reg [7:0] s ; // vector
-  output cout ;
   reg [7:0] s0, s1, s2, s3, s4;
 
   //function declerations
-  add8 adde(s0, cout, ci, a, b);
-  sub8 sube(s1, cout, ci, a, b);
+  add8 adde(s0, a, b);
+  sub8 sube(s1, a, b);
   and8 ande(s2,a,b);
   or8 ore(s3,a,b);
   not8 note(s4, a);
 
 
-  always @(a,b,ci,aluMode) begin
+  always @(*) begin
     if(aluMode==4'b0000) begin//add
       s <= s0;
     end
--- zALUTest.sv
+++ zALUTest.sv
@@ -5,20 +5,17 @@ module test;
   
   reg [7:0] a;
   reg [7:0] b;
-  reg ci;
   reg [3:0] aluMode;
   reg [7:0] s;
-  reg cout;
   
   //initiate add8
-  alu myAlu(s,cout, ci, a, b, aluMode);
+  alu myAlu(s, a, b, aluMode);
   
   initial begin
     $dumpfile("dump.vcd");
     $dumpvars(1,test);
  	
     aluMode = 4'b0000;
-    ci = 1'b0;
     a = 0'hab;
     b = 0'hcb;
     
@@ -37,7 +34,6 @@ module test;
     #10;
     
     aluMode = 4'b0000;
-    ci = 1'b0;
     a = 0'h6f;
     b = 0'he1;
     
