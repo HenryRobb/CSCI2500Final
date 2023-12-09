b3413e55b0936b39adc2a0a5fdc1b37424f28cd7
ColbyLairdieson <lairdc@rpi.edu>
2023-12-08 23:38:38 -0500

    Update factorial.asm
    added address

--- factorial.asm
+++ factorial.asm
@@ -6,37 +6,41 @@
 /Step 5: Halt if B = 0
 /Step 6: Jump back to Step 1
     
-    Load        N          / Load N into AC
-    Sub         Pos1
-    Store       Num        / Store the number of loop iterations in loop control variable
 
-Loop2,Load Num           /Setting the Ctr to be equal to Num
-    Store Ctr
 
-Loop1, Load    Prod       / Load the product into AC
-    Add       N          / Add the multiplicand
-    Store     Prod       / Store the new product
-    Load      Ctr        / Load the loop control variable
-    Sub       Pos1       / Decrement the loop control variable by one
-    Store     Ctr        / Store the new value of the loop control variable
-    Skipcond  400        / If control variable = 0, skip next instruction to terminate the loop
-    Jump      Loop1       / Otherwise, go to Loop
+FACT
+Address     Value       Code    
+00-01    Load        N          / Load N into AC
+02-03    Sub         Pos1
+04-05    Store       Num        / Store the number of loop iterations in loop control variable
 
-    Load Num             /Loads Num into AC to Decrment it one
-    Sub         Pos1     /Decrement one
-    Store       Num      /Stores back into num
-    Load        Prod     /This loads Product back into N so N can be a multiplicand
-    Store       N           
-    Load        Zero     /Resetting Product to be 0 for next loop or multiply
-    Store       Prod     
-    Load        Num      /Checking that num isnt 0
-    Skipcond    400
-    Jump        Loop2    /Jumping back start next multiply
-    Halt
+06-07    Loop2,Load Num           /Setting the Ctr to be equal to Num
+08-09    Store Ctr
 
-N,    Dec     5          / N is N in N!
-Num,    Dec   0        / Num is N-1 to 
-Prod, Dec     0          / The product
-Ctr,  Hex     0          / The loop control variable
-Pos1, Dec     1         / Used to increment and decrement by 1
-Zero, Dec     0
+0A-0B   Loop1, Load    Prod       / Load the product into AC
+0C-0D    Add       N          / Add the multiplicand
+0E-0F    Store     Prod       / Store the new product
+10-11    Load      Ctr        / Load the loop control variable
+12-13    Sub       Pos1       / Decrement the loop control variable by one
+14-15    Store     Ctr        / Store the new value of the loop control variable
+16-17    Skipcond  400        / If control variable = 0, skip next instruction to terminate the loop
+18-19    Jump      Loop1       / Otherwise, go to Loop
+
+1A-1B    Load Num             /Loads Num into AC to Decrment it one
+1C-1D    Sub         Pos1     /Decrement one
+1E-1F    Store       Num      /Stores back into num
+20-21    Load        Prod     /This loads Product back into N so N can be a multiplicand
+22-23    Store       N           
+24-25    Load        Zero     /Resetting Product to be 0 for next loop or multiply
+26-27    Store       Prod     
+28-29    Load        Num      /Checking that num isnt 0
+2A-2B    Skipcond    400
+2C-2D    Jump        Loop2    /Jumping back start next multiply
+2E-2F    Halt
+
+30      N,    Dec     5          / N is N in N!
+31      Num,    Dec   0        / Num is N-1 to 
+32      Prod, Dec     0          / The product
+33      Ctr,  Hex     0          / The loop control variable
+34      Pos1, Dec     1         / Used to increment and decrement by 1
+35      Zero, Dec     0
